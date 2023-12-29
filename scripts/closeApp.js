7b9a5e5d61c1814be3cb5bec0921d51ad2e667c9
lalt22 <lalitha2seshadri@gmail.com>
2023-12-29 16:00:24 +1100

    Modals are openable and closable

---
+++ scripts/closeApp.js
@@ -0,0 +1,29 @@
+function closeFinder() {
+    console.log("Closing Finder");
+    var finderModal = document.getElementById("finderModal");
+    finderModal.style.display = "none";
+}
+
+function closeNotes() {
+    console.log("Closing Notes");
+    var notesModal = document.getElementById("notesModal");
+    notesModal.style.display = "none";
+}
+
+function closeTerminal() {
+    console.log("Closing Terminal");
+    var terminalModal = document.getElementById("terminalModal");
+    terminalModal.style.display = "none";
+}
+
+function closeFaceTime() {
+    console.log("Closing Facetime");
+    var facetimeModal = document.getElementById("facetimeModal");
+    facetimeModal.style.display = "none";
+}
+
+function closeCalculator() {
+    console.log("Closing Calculator");
+    var calculatorModal = document.getElementById("calculatorModal");
+    calculatorModal.style.display = "none";
+}
---
+++ scripts/openApp.js
@@ -0,0 +1,33 @@
+
+
+
+
+function openFinder() {
+    console.log("Opening Finder");
+    var finderModal = document.getElementById("finderModal");
+    finderModal.style.display = "block";
+}
+
+function openNotes() {
+    console.log("Opening Notes");
+    var notesModal = document.getElementById("notesModal");
+    notesModal.style.display = "block";
+}
+
+function openTerminal() {
+    console.log("Opening Terminal");
+    var terminalModal = document.getElementById("terminalModal");
+    terminalModal.style.display = "block";
+}
+
+function openFaceTime() {
+    console.log("Opening Facetime");
+    var facetimeModal = document.getElementById("facetimeModal");
+    facetimeModal.style.display = "block";
+}
+
+function openCalculator() {
+    console.log("Opening Calculator");
+    var calculatorModal = document.getElementById("calculatorModal");
+    calculatorModal.style.display = "block";
+}
